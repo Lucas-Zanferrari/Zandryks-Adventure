9498f8cdc1a03bff8999391b41c513c26b315fb9
crusuer <robson_mt@hotmail.com>
2017-05-02 00:45:25 -0300

    Correcao quando timer zera

--- Assets/_Scripts/TimerManager.cs
+++ Assets/_Scripts/TimerManager.cs
@@ -17,6 +17,9 @@ public class TimerManager : MonoBehaviour {
         theText.text = (int)(startingTime / 60) + ":" + Mathf.Round(startingTime%60).ToString("00");
         if(startingTime <= 0) 
         {
+            GameObject relativesFoundKeeper = GameObject.Find("RelativesFound").gameObject;
+            //relativesFoundKeeper.GetComponent<RelativesFoundKeeper>().AddFoundRelative(gameObject.transform.name);
+            DontDestroyOnLoad(relativesFoundKeeper);
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             // #if UNITY_EDITOR
                 // UnityEditor.EditorApplication.isPlaying = false;
